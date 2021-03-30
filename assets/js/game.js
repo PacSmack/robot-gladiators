752d7972c07a92b244b086e7e7e498bd68e35d5f
Nathan da Silva <pacsmacck@gmail.com>
2021-03-30 15:54:45 -0700

    fixed fight-skip bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,27 +8,42 @@ var randomNumber = function (min, max) {
 };
 
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
+
+
 var fight = function (enemy) {
-    while (playerInfo.health > 0 && enemy.health > 0) {
-        // ask player if they'd like to fight or run
+    // ask player if they'd like to fight or run
+    var fightOrSkip = function () {
+        // ask player if they'd like to fight or skip using fightOrSkip function
         var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
+        if (promptFight === "" || promptFight === null) {
+            window.alert("You need to provide a valid answer! Please try again.");
+            return fightOrSkip();
+        }
+
         // if player picks "skip" confirm and then stop the loop
-        if (promptFight === "skip" || promptFight === "SKIP") {
+        promptFight = promptFight.toLowerCase();
+        if (promptFight === "skip") {
             // confirm player wants to skip
             var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
             // if yes (true), leave fight
             if (confirmSkip) {
-                window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-                // subtract money from playerInfo.money for skipping
+                window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+                // subtract money from playerMoney for skipping
                 playerInfo.money = Math.max(0, playerInfo.money - 10);
-                console.log("playerInfo.money", playerInfo.money)
-                break;
+                return true;                
             }
         }
+    }
 
-        // generate random damage value based on player's attack power
+
+    // generate random damage value based on player's attack power
+    while (playerInfo.health > 0 && enemy.health > 0) {
+        if (fightOrSkip()) {
+            // if true, leave fight by breaking loop
+            break;
+        }
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
         enemy.health = Math.max(0, enemy.health - damage);
@@ -68,6 +83,7 @@ var fight = function (enemy) {
         }
     }
 };
+
 
 // function to start a new game
 var startGame = function () {
@@ -167,7 +183,7 @@ var shop = function () {
 /* END GAME FUNCTIONS */
 
 /* GAME INFORMATION / VARIABLES */
-var getPlayerName = function() {
+var getPlayerName = function () {
     var name = "";
     while (name === "" || name === null) {
         name = prompt("What is your robot's name?");
@@ -178,7 +194,7 @@ var getPlayerName = function() {
 }
 
 // player information
-var playerInfo = {    
+var playerInfo = {
     name: getPlayerName(),
     health: 100,
     attack: 10,
